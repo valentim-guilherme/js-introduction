2163334063d24677e53c31ffb142a709178be60f
Guilherme Valentim <valentim.guilherme@gmail.com>
2019-11-10 16:45:45 -0300

    Arrow functions

--- bundle.js
+++ bundle.js
@@ -1,19 +1,24 @@
 "use strict";
 
-var arr = [1, 3, 4, 5, 8, 9];
-var newArr = arr.map(function (item, index) {
-  return item + index;
+var arr = [1, 3, 4, 5, 6]; // const newArr = arr.map(function(item){
+//     return item * 2;
+// });
+// const newArr = arr.map((item) => {
+//     return item * 2;
+// });
+// const newArr = arr.map(item => {
+//     return item * 2;
+// });
+
+var newArr = arr.map(function (item) {
+  return item * 2;
 });
 console.log(newArr);
-var sum = arr.reduce(function (total, next) {
-  return total + next;
-});
-console.log(sum);
-var filter = arr.filter(function (item) {
-  return item % 2 === 0;
-});
-console.log(filter);
-var find = arr.find(function (item) {
-  return item === 2;
-});
-console.log(find);
+
+var teste = function teste() {
+  return {
+    name: 'Guilherme'
+  };
+};
+
+console.log(teste());
--- main.js
+++ main.js
@@ -1,27 +1,21 @@
-const arr = [1, 3, 4, 5, 8, 9];
+const arr = [1, 3, 4, 5, 6];
 
-const newArr = arr.map(function(item, index){
-    return item + index;
-})
+// const newArr = arr.map(function(item){
+//     return item * 2;
+// });
+
+// const newArr = arr.map((item) => {
+//     return item * 2;
+// });
+
+// const newArr = arr.map(item => {
+//     return item * 2;
+// });
+
+const newArr = arr.map(item => item * 2);
 
 console.log(newArr);
 
-const sum = arr.reduce(function(total, next){
-    return total + next;
-})
+const teste = () => ({ name: 'Guilherme' });
 
-console.log(sum);
-
-
-const filter = arr.filter(function(item){
-    return item % 2 === 0;
-})
-
-console.log(filter);
-
-
-const find = arr.find(function(item){
-    return item === 2;
-})
-
-console.log(find);
+console.log(teste());
