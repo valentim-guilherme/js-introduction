0aab2aa04df508997daf9899f204df7bd2ba071b
Guilherme Valentim <valentim.guilherme@gmail.com>
2019-11-10 16:14:21 -0300

    class

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
-class Teste {
-    metodo(){
-
+class List {
+    constructor(){
+        this.data = [];
     }
 
-    outro(){
-        // dsauhdsuihdiasu
+    add(data){
+        this.data.push(data);
+        console.log(data);
     }
-    outro2(){
-        // dsauhdsuihdiasu
+}
+
+
+class TodoList extends List {
+    constructor(){
+        super();
+
+        this.usuario = 'Guilherme';
     }
+
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+}
+
+var MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function(){
+    MinhaLista.add('Novo todo 2');
+    MinhaLista.mostraUsuario('Novo todo 2');
 }
